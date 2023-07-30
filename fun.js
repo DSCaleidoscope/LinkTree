1a08e3b2ba691cdf93b57844f81667d320b8de55
D. S. Caleidoscope <129209814+DSCaleidoscope@users.noreply.github.com>
2023-07-30 12:40:51 +0200

    Update fun.js

--- fun.js
+++ fun.js
@@ -1,11 +1,22 @@
 function g(id){return document.getElementById(id);}
 function clear(){g("bbody").innerHTML = '';}
+function add(t){g("bbody").innerHTML += t;}
 function parse(t){branches = JSON.parse(t);console.log(branches);}
 
 function fill(k){
   clear();
   readTextFile(k);
   setTimeout("parse(allText)", 1000);
+  addElements();
+}
+
+function addElements(){
+  let i = 0;
+  let x = branches.length;
+
+  for(;i < x;i++){
+    add("<div id='' class='branch'><div class='title'>" + branches[i].getContent() + "</div></div>");
+  }
 }
 
 function readTextFile(file){
@@ -28,4 +39,18 @@ function readTextFile(file){
   rawFile.send(null);
 
   return null;
+}
+
+
+branch.getContent = function(){
+  if(this.type === "email"){
+    console.log("email");
+    return "<a href='" + this.value + "'>" + this.title + "</a>";
+  }else if(this.type === "link"){
+    console.log("link");
+    return "<a href='" + this.value + "'>" + this.title + "</a>";
+  }else{
+    console.log("Unknown type: " + this.type);
+    return "";
+  }
 }
